cd1ac3cade9012562c5b7fe90638eb9ff211a2f4
Abing <97945597+nabilrmdaan@users.noreply.github.com>
2022-02-19 13:27:33 +0700

    Update chatbot.py

--- EmikoRobot/modules/chatbot.py
+++ EmikoRobot/modules/chatbot.py
@@ -434,7 +434,7 @@ async def inuka(client, message):
 
 
 __help__ = """
-❂ Emiko AI is the only ai system which can detect & reply upto 200 language's
+❂ Abing AI is the only ai system which can detect & reply upto 200 language's
 
 ❂ /chatbot [ON/OFF]: Enables and disables AI Chat mode.
 ❂ /chatbot EN : Enables English only chatbot.
