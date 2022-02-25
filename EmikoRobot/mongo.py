7cdfd863a6c9a27abce0b442f4bf76a704a4bdf4
Abing <97945597+nabilrmdaan@users.noreply.github.com>
2022-02-25 13:52:44 +0700

    Update mongo.py

--- EmikoRobot/mongo.py
+++ EmikoRobot/mongo.py
@@ -9,14 +9,14 @@ from EmikoRobot.confing import get_int_key, get_str_key
 
 MONGO_PORT = get_int_key("27017")
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "EmikoRobot"
+MONGO_DB = "AbingRobot"
 
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
-db = client["emiexrobot"]
+db = client["abingxrobot"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
